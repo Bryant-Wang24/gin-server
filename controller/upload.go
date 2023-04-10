76f765f189868877873b6a5a49e57a2ed3f02231
Bryant-Wang24 <485969746@qq.com>
2023-04-11 00:06:11 +0800

    :sparkles: upload images working in process

--- controller/upload.go
+++ controller/upload.go
@@ -2,11 +2,36 @@ package controller
 
 import (
 	"fmt"
+
 	"github.com/gin-gonic/gin"
 )
 
 // UploadFile 上传文件
 func UploadFile(c *gin.Context) {
-	file, _ := c.FormFile("file")
+	file, err := c.FormFile("file")
+	if err != nil {
+		c.JSON(200, gin.H{
+			"code": 100,
+			"msg":  "上传文件失败",
+		})
+		return
+	}
 	fmt.Println(file.Filename)
+	// 保存文件
+	err = c.SaveUploadedFile(file, "images/logo")
+	if err != nil {
+		fmt.Println("上传文件失败", err)
+		c.JSON(200, gin.H{
+			"code": 100,
+			"msg":  "上传文件失败",
+		})
+		return
+	}
+	c.JSON(200, gin.H{
+		"code": 200,
+		"msg":  "上传文件成功",
+		"data": []gin.H{
+			{"url": "https:wangqiushuang.online:8080/logo/" + file.Filename},
+		},
+	})
 }
